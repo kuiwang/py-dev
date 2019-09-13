9bba2046ee5ad5e1de27427cacca041bfb91b08e
st#hme <wk@gmail.com>
2019-09-13 10:19:32 +0800

    add balance check

---
+++ src/com/scott/dev/demo/btc/balance_check.py
@@ -0,0 +1,228 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 2018年12月14日
+获得top address,
+因使用了br编码，引入了 brotli包
+@author: user
+'''
+import os, sys
+import logging
+import requests, json
+# from com.scott.dev.util.mysqlpool import MySQLConnPool
+from bs4 import BeautifulSoup 
+from importlib import reload
+import socks, socket
+import random
+import brotli
+
+reload(sys)  
+# sys.setdefaultencoding('utf8')
+
+PY_GEN_PATH = "D:/download/pygen/bitauto".replace('/', os.sep)
+MULTI_API = "https://blockchain.info/multiaddr?active={}"
+SINGLE_API = "https://blockchain.info/rawaddr/{}"
+BTC_API_MULTI = "https://chain.api.btc.com/v3/address/{}"
+addr_file = 'D:/download/addr'
+logger = logging.getLogger('blockchain_api')
+LOG_FILE = 'blockchain_api.log'
+# LOG_FORMATTER = '%(asctime)s - %(filename)s - %(funcName)s - %(lineno)d - %(threadName)s - %(process)d - %(name)s - %(levelname)s - %(message)s'
+# LOG_FORMATTER = '%(asctime)s-%(levelname)s - %(filename)s - %(funcName)s - %(lineno)d - %(message)s'
+LOG_FORMATTER = '%(message)s'
+'''
+#sock5 proxy
+SOCKS5_PROXY_HOST = '127.0.0.1'  # socks 代理IP地址
+SOCKS5_PROXY_PORT = 10900  # socks 代理本地端口
+default_socket = socket.socket
+socks.set_default_proxy(socks.SOCKS5, SOCKS5_PROXY_HOST, SOCKS5_PROXY_PORT)
+socket.socket = socks.socksocket
+'''
+s = requests.Session()
+
+
+def get_url(url, refer, proxy=None):
+    try:
+        UA_LST = ['Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+              'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',
+              'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; rv:11.0) like Gecko)',
+              'Mozilla/5.0 (Windows; U; Windows NT 5.2) Gecko/2008070208 Firefox/3.0.1',
+              'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070309 Firefox/2.0.0.3',
+              'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070803 Firefox/1.5.0.12',
+              'Opera/9.27 (Windows NT 5.2; U; zh-cn)',
+              'Mozilla/5.0 (Macintosh; PPC Mac OS X; U; en) Opera 8.0',
+              'Opera/8.0 (Macintosh; PPC Mac OS X; U; en)',
+              'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.12) Gecko/20080219 Firefox/2.0.0.12 Navigator/9.0.0.6',
+              'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0)',
+              'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)',
+              'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E)',
+              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Maxthon/4.0.6.2000 Chrome/26.0.1410.43 Safari/537.1 ',
+              'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E; QQBrowser/7.3.9825.400)',
+              'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0 ',
+              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.92 Safari/537.1 LBBROWSER',
+              'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; BIDUBrowser 2.x)',
+              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/3.0 Safari/536.11']
+        ACCEPT = "application/json"
+        ACCEPT_ENCODING = "gzip, deflate, br"
+        ACCEPT_LANGUAGE = "zh-CN,zh;q=0.9"
+        CONNECTION = "keep-alive"
+        HOST = "blockchain.info"
+        header = {"HOST":HOST, "METHOD":"GET", "User-Agent":UA_LST[random.randrange(0, len(UA_LST))], 'Cache-Control':'no-cache', 'Referer':refer, "Accept":ACCEPT, "Accept-Encoding":ACCEPT_ENCODING, "Accept-Language":ACCEPT_LANGUAGE, "Connection":CONNECTION}
+        r = s.get(url, headers=header)
+        sc = r.status_code
+        if sc == 200:
+            r.encoding = 'utf-8'
+            '''
+            logger.info('headers in below:\n')
+            header_lst = r.headers
+            for k, v in header_lst.items():
+                logger.info(k + "=" + v)
+            logger.info('encoding:\n' + r.encoding)
+            '''
+            key = 'Content-Encoding'
+            # print(response.headers[key])
+            if(key in r.headers and r.headers['Content-Encoding'] == 'br'):
+                data = brotli.decompress(r.content)
+                data1 = data.decode('utf-8')
+                # logger.info('data1：' + data1)
+                return data1
+            # logger.info('r.text:\n' + r.text)
+            return r.text
+        else:
+            logger.error('response code:{}'.format(str(sc)))
+            return None
+    except Exception as e:
+        logger.error(e)
+        return None
+
+
+def config_logger():
+    logger.setLevel(logging.DEBUG)
+    if not os.path.exists(PY_GEN_PATH):
+        logger.info("文件夹不存在,已自行创建")
+        os.makedirs(PY_GEN_PATH, 777)
+    handler = logging.FileHandler(os.path.join(PY_GEN_PATH, LOG_FILE))
+    handler.setLevel(logging.DEBUG)
+    fmter = logging.Formatter(LOG_FORMATTER)
+    handler.setFormatter(fmter)
+    logger.addHandler(handler)
+
+    # 控制台打印
+    console = logging.StreamHandler()
+    console.setLevel(level=logging.DEBUG)  # 设置为INFO级别
+    console.setFormatter(fmter)
+    logger.addHandler(console)
+
+
+def singleGetBalanceInfo():
+    logger.info("singleGetBalanceInfo start")
+    i = 0
+    with open(addr_file, 'r') as f:
+        addr = f.readline().strip()
+        while addr:
+            i = i + 1
+            
+            single_qry = SINGLE_API.format(str(addr))
+            resp_json = get_url(single_qry, single_qry)
+            addr_info = json.loads(resp_json)
+            balance = addr_info['final_balance']
+            logger.info('{} | balance:{}'.format(str(addr), str(balance)))
+            addr = f.readline().strip()
+
+
+def batchGetBalanceInfo():
+    # logger.info("batchGetBalanceInfo start")
+    param = []
+    i = 0
+    with open(addr_file, 'r') as f:
+        addr = f.readline().strip()
+        while addr:
+            i = i + 1
+            param.append(addr)
+            size = len(param)
+            if(size % 50 == 0):
+                qry = '|'.join(param)
+                batch_api = MULTI_API.format(str(qry))
+                resp_json = get_url(batch_api, batch_api)
+                batch_addr_info = json.loads(resp_json)
+                address_lst = batch_addr_info['addresses']
+                for address in address_lst:
+                    a = address['address']
+                    b = address['final_balance']
+                    if int(b) > 0:
+                        logger.info('{} | balance:{}'.format(str(a), str(b)))
+                    else:
+                        logger.info('{}'.format(str(a)))
+                param = []
+            addr = f.readline().strip()
+        size = len(param)
+        if size>0:
+            qry = '|'.join(param)
+            batch_api = MULTI_API.format(str(qry))
+            resp_json = get_url(batch_api, batch_api)
+            batch_addr_info = json.loads(resp_json)
+            address_lst = batch_addr_info['addresses']
+            for address in address_lst:
+                a = address['address']
+                b = address['final_balance']
+                if int(b) > 0:
+                    logger.info('{} | balance:{}'.format(str(a), str(b)))
+                else:
+                    logger.info('{}'.format(str(a)))
+            param = []
+
+
+def batchGetBalanceInfoViaBTC():
+    # logger.info("batchGetBalanceInfo start")
+    param = []
+    addr_file1 = 'D:/download/addr'
+    with open(addr_file1, 'r') as f:
+        addr = f.readline().strip()
+        while addr:
+            param.append(addr)
+            size = len(param)
+            if(size % 10 == 0):
+                qry = ','.join(param)
+                batch_api = BTC_API_MULTI.format(str(qry))
+                resp_json = get_url(batch_api, batch_api)
+                batch_addr_info = json.loads(resp_json)
+                data = batch_addr_info['data']
+                for d in data:
+                    a = d['address']
+                    b = d['balance']
+                    if int(b) > 0:
+                        logger.info('{} | balance:{}'.format(str(a), str(b)))
+                    else:
+                        logger.info('{}'.format(str(a)))
+                param = []
+            addr = f.readline().strip()
+        size = len(param)
+        if size>0:
+            qry = ','.join(param)
+            batch_api = BTC_API_MULTI.format(str(qry))
+            resp_json = get_url(batch_api, batch_api)
+            batch_addr_info = json.loads(resp_json)
+            data = batch_addr_info['data']
+            for d in data:
+                a = d['address']
+                b = d['balance']
+                if int(b) > 0:
+                    logger.info('{} | balance:{}'.format(str(a), str(b)))
+                else:
+                    logger.info('{}'.format(str(a)))
+            param = []
+
+
+def test():
+    a = ['123', '456', '789', '012', '234']
+    r = '|'.join(a)
+    logger.info(r)
+    logger.info(type(r))
+
+
+if __name__ == '__main__':
+    config_logger()
+    # test()
+    batchGetBalanceInfo()
+    '''
+    url = 'https://bitinfocharts.com/top-100-richest-bitcoin-addresses-1.html'
+    parseBtcAnalyticsPage(url)
+    '''
